fd9ecb57e072482bed30e6fabb4a8ac69f5d076d
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2025-03-22 15:00:09 -0300

    feat: :sparkles: criado a lógica de duelo

--- src/index.js
+++ src/index.js
@@ -2,33 +2,16 @@ import { PersonagemView } from './components/personagem-view.js';
 import { Mago } from './modules/mago.js';
 import { Arqueiro } from './modules/arqueiro.js';
 import { ArqueiroMago } from './modules/arqueiro-mago.js';
+import { Personagem } from './modules/personagem.js';
 
-const personagemNovo = new Mago(
-  'Filipe',
-  7,
-  'O mago supremo em termos de magia.',
-  'Sombras',
-  7,
-  6
-);
+const personagemNovo = new Mago('Filipe', 7, 'Sombras', 7, 6);
 
-const personagemDois = new Arqueiro(
-  'Flecha',
-  4,
-  'A aprendiz do mago supremo. Ela busca criar flechas mágicas.',
-  6
-);
+const personagemDois = new Arqueiro('Ana', 4, 6);
 
-const arqueiroMago = new ArqueiroMago(
-  'Chico',
-  7,
-  'O lendário Arqueiro-Mago.',
-  10,
-  'Ar',
-  4,
-  8
-);
+const arqueiroMago = new ArqueiroMago('Chico', 7, 10, 'Ar', 4, 8);
 
 const personagens = [personagemNovo, personagemDois, arqueiroMago];
 
 new PersonagemView(personagens).render();
+
+console.log(Personagem.duelar(personagemNovo, personagemDois));
