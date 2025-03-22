24f1019c29af03776f0eb98ac42d290e514f6d70
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2025-03-22 16:14:44 -0300

    feat: :sparkles: criado a classe guerreira e renderizada

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { PersonagemView } from './components/personagem-view.js';
 import { Mago } from './modules/mago.js';
 import { Arqueiro } from './modules/arqueiro.js';
 import { ArqueiroMago } from './modules/arqueiro-mago.js';
-import { Personagem } from './modules/personagem.js';
+import { Guerreiro } from './modules/guerreiro.js';
 
 const personagemNovo = new Mago('Filipe', 'Sombras', 7, 6);
 
@@ -10,8 +10,8 @@ const personagemDois = new Arqueiro('Ana', 6);
 
 const arqueiroMago = new ArqueiroMago('Chico', 10, 'Ar', 4, 8);
 
-const personagens = [personagemNovo, personagemDois, arqueiroMago];
+const guerreiro = new Guerreiro('Daniela', 18);
+
+const personagens = [personagemNovo, personagemDois, arqueiroMago, guerreiro];
 
 new PersonagemView(personagens).render();
-
-console.log(Personagem.verificarVencedor(personagemNovo, personagemDois));
