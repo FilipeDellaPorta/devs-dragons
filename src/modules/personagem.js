72971a38c91d6ec0652e807d1c26a75718ad8d77
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2025-03-22 15:53:14 -0300

    feat: :sparkles: encapsulado a propriedade level

--- src/modules/personagem.js
+++ src/modules/personagem.js
@@ -2,14 +2,14 @@ export class Personagem {
   nome;
   vida = 100;
   mana = 100;
-  level;
+  #level;
 
   constructor(nome, level) {
-    (this.nome = nome), (this.level = level);
+    (this.nome = nome), (this.#level = level);
   }
 
   obterInsignia() {
-    if (this.level >= 5) {
+    if (this.#level >= 5) {
       return `Implacável ${this.constructor.tipo}`;
     }
     return `${this.constructor.tipo} iniciante`;
